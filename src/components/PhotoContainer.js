bc6c82e7638f5ecf4f693d77efb630af437998cf
L. Bennett Crantford <lcranfrd@comcast.net>
2021-05-07 17:33:32 -0400

    Update PhotoContainer.js

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,24 +1,27 @@
+
 import React from 'react';
 import PhotoBuild from './PhotoBuild';
-
 const PhotoContainer = ((props) => {
   const {data, title} = props;
   return(
     <div className="photo-container">
       <h2>{title}</h2>
-      <ul>
-      {
-        [...data].map((v) => 
-          <PhotoBuild 
-            serverId={v.server}
-            id={v.id}
-            secret={v.secret}
-            title={v.title}
-            key={v.id}
-          />
-        )
+      {data.length === 0
+        ? <h3>Your Search Did Not Return Any Results</h3>
+        :<ul>
+        {
+          [...data].map((v) => 
+            <PhotoBuild 
+              serverId={v.server}
+              id={v.id}
+              secret={v.secret}
+              title={v.title}
+              key={v.id}
+            />
+          )
+        }
+        </ul>
       }
-      </ul>
     </div>
   );
 })
