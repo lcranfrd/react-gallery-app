32918876fa50bd67e3a3d805eb313c5ad7382320
L. Bennett Crantford <lcranfrd@comcast.net>
2021-05-18 15:04:00 -0400

    fixed dependancies

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const NotFound = (props) => {
-  console.log(props)
   return(
   <li className ="not-found">
     <h2>{`Page "${props.location.pathname}" Not Found!`}</h2>
